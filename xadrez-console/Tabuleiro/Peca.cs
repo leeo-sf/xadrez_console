ce5c2771264f8878bebb25bd62036cd2c3f8815d
Leonardo Silva <leonardoferreira032@gmail.com>
2022-07-23 16:42:55 -0300

    Comecando a implementar o tabuleiro

---
+++ xadrez-console/Tabuleiro/Peca.cs
@@ -0,0 +1,22 @@
+﻿namespace tabuleiro
+{
+    class Peca
+    {
+        // Toda peça tem uma posição
+        public Posicao posicao { get; set; }
+        // Toda peça tem uma cor {pode ser acessada e alterada por ela mesmo ou por subclasses}
+        public Cor cor { get; protected set; }
+        // Quantidade de vezes que a peça se movimentou
+        public int qtdMovimentos { get; protected set; }
+        // Toda peça está em um tabuleiro {pode ser acessada e alterada por ela mesmo ou por subclasses}
+        public Tabuleiro tab { get; set; }
+
+        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        {
+            this.posicao = posicao;
+            this.tab = tab;
+            this.cor = cor;
+            this.qtdMovimentos = 0;
+        }
+    }
+}
---
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,19 @@
+﻿namespace tabuleiro
+{
+    public class Tabuleiro
+    {
+        // Todo tabuleiro tem linhas e colunas
+        public int linha { get; set; }
+        public int coluna { get; set; }
+        // O xadrez tem uma matriz de peças
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            this.linha = linhas;
+            this.coluna = colunas;
+            // As peças tera o número de linhas pelo número de colunas informadas na construtor
+            this.pecas = new Peca[linhas, colunas];
+        }
+    }
+}
