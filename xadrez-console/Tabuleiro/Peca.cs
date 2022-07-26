efa2ebb254f9e70ae0c24ffd448e5814f3f259fd
Leonardo Silva <leonardoferreira032@gmail.com>
2022-07-26 18:51:15 -0300

    Implementando a verificacao de xeque

--- xadrez-console/Tabuleiro/Peca.cs
+++ xadrez-console/Tabuleiro/Peca.cs
@@ -24,6 +24,11 @@
             qtdMovimentos++;
         }
 
+        public void decrementarQtdMovimentos()
+        {
+            qtdMovimentos--;
+        }
+
         public bool existeMovimentosPossiveis()
         {
             bool[,] mat = movimentosPossiveis();
